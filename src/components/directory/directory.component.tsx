81644c17ed37e2201bb9ab6d632793ffb8108a1a
Gilxhon Hima <gilxhonhima@gmail.com>
2024-01-29 21:59:23 +0100

    updating directory page with directory component and category item component

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,20 @@
+import CategoryItem from "../category-item/category-item.component.tsx";
+import "./directory.styles.scss"
+import {Category} from "../../types/interfaces.ts";
+import React from "react";
+
+interface DirectoryProps {
+    categories: Category[]
+}
+
+const Directory: React.FC<DirectoryProps> = ({categories}) => {
+    return (
+        <div className="categories-container">
+            {categories.map((category: Category) => (
+                <CategoryItem key={category.id} category={category}/>
+            ))}
+        </div>
+    )
+}
+
+export default Directory;
